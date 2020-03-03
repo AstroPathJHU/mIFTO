bd624dada26b0288f4f3ece07d3c0e7c8b8fa3fe
ssotodi1 <54324393+ssotodi1@users.noreply.github.com>
2020-03-03 13:12:44 -0500

    Fixed reshape2 typo in line 245

--- R/FOP.R
+++ R/FOP.R
@@ -242,7 +242,7 @@ findposFOP<-function(Positive.table,out,Slide_Descript,fraction.type){
           count3, CellSeg$`Sample Name`)}
       ##find positive cells and generate output file
       ##Positive_cells data.table can be added to for additional AB with the same SlideIDs.
-      Positive.table<-rbind(Positive.table,resahpe2::dcast(dplyr::mutate(dplyr::summarise(dplyr::group_by(
+      Positive.table<-rbind(Positive.table,reshape2::dcast(dplyr::mutate(dplyr::summarise(dplyr::group_by(
         CellSeg, `Sample Name`,Concentration),
         Total.Tumor.Area = sum(`Tumor`), Total.NonTumor.Area = sum(`Non Tumor`)),
         Fraction=(Total.Tumor.Area/(Total.NonTumor.Area+Total.Tumor.Area))),
