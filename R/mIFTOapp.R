8f9f1621770678ec1c89b8cd4b825ecc37e1435b
ssotodi1 <54324393+ssotodi1@users.noreply.github.com>
2022-07-20 10:23:58 -0400

    Update mIFTOapp.R

--- R/mIFTOapp.R
+++ R/mIFTOapp.R
@@ -50,20 +50,10 @@ mIFTOapp <- function(){
     ip = "127.0.0.1"
   })
   #
-  tryCatch({
-    options(browser = "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe")
+  options(browser = "C:/Program Files (x86)/Google/Chrome/Application/chrome.exe")
     shiny::shinyApp(ui = mIFTO::ui.map(), mIFTO::server.side,
                   options = list(width = 1000, launch.browser = TRUE,
                                  host = ip, quiet = T))
-  }, warning = function(cond) {
-    tryCatch({
-    options(browser = "C:/Program Files (x86)/Internet Explorer/iexplore.exe")
-    shiny::shinyApp(ui = mIFTO::ui.map(), mIFTO::server.side,
-                    options = list(width = 1000, launch.browser = TRUE,
-                                   host = ip, quiet = T))
-    },  warning = function(cond) {
-      stop('Error could not find supported web browser.')
-    })
     #
   })
   #
