b98c0ed17f2d21d5d4095e3d11254be436af0544
ssotodi1 <54324393+ssotodi1@users.noreply.github.com>
2022-07-26 09:18:46 -0400

    parallel

--- R/parallel.invoke.gpxp.R
+++ R/parallel.invoke.gpxp.R
@@ -86,14 +86,16 @@ parallel.invoke.gpxp <- function (
   print(decile.logical)
   print("threshold.logical")
   print(threshold.logical)
+  print("Image.IDs[[x]][[y]]")
+  print(Image.IDs[[x]][[y]])
   ###### need to add a try catch, but also need to determine what happens
   ###### when I throw an error instead of the envir
     small.tables.byimage<- parallel::parLapply(
-      cl,Image.IDs[[x]][[y]],function(z) mIFTO::generate.pxp.image.data(
-        Concentration, x, y, z, Antibody_Opal,
-        titration.type.name, Thresholds, paths,
-        connected.pixels, flowout, Opal1,
-        decile.logical, threshold.logical))
-  #
+      cl,Image.IDs[[x]][[y]],function(z) z*10)
+  #mIFTO::generate.pxp.image.data(
+# Concentration, x, y, z, Antibody_Opal,
+# titration.type.name, Thresholds, paths,
+# connected.pixels, flowout, Opal1,
+# decile.logical, threshold.logical)
   print("03")
 }
