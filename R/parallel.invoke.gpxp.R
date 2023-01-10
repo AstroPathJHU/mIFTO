6a882d8a6c595bb5a062f604d144248abf734874
ssotodi1 <54324393+ssotodi1@users.noreply.github.com>
2023-01-10 11:27:46 -0500

    function

--- R/parallel.invoke.gpxp.R
+++ R/parallel.invoke.gpxp.R
@@ -39,6 +39,55 @@ parallel.invoke.gpxp <- function (
   titration.type.name, Thresholds, paths,
   connected.pixels, flowout, Opal1,
   decile.logical, threshold.logical, cl){
+  withJavaLogging <- function(expr, silentSuccess=FALSE, stopIsFatal=TRUE) {
+    hasFailed = FALSE
+    messages = list()
+    warnings = list()
+    logger = function(obj) {
+      # Change behaviour based on type of message
+      level = sapply(class(obj), switch, debug="DEBUG", message="INFO", warning="WARN", caughtError = "ERROR",
+                     error=if (stopIsFatal) "FATAL" else "ERROR", "")
+      level = c(level[level != ""], "ERROR")[1]
+      simpleMessage = switch(level, DEBUG=,INFO=TRUE, FALSE)
+      quashable = switch(level, DEBUG=,INFO=,WARN=TRUE, FALSE)
+
+      # Format message
+      time  = format(Sys.time(), "%Y-%m-%d %H:%M:%OS3")
+      txt   = conditionMessage(obj)
+      if (!simpleMessage) txt = paste(txt, "\n", sep="")
+      msg = paste(time, level, txt, sep=" ")
+      calls = sys.calls()
+      calls = calls[1:length(calls)-1]
+      trace = limitedLabels(c(calls, attr(obj, "calls")))
+      if (!simpleMessage && length(trace) > 0) {
+        trace = trace[length(trace):1]
+        msg = paste(msg, "  ", paste("at", trace, collapse="\n  "), "\n", sep="")
+      }
+
+      # Output message
+      if (silentSuccess && !hasFailed && quashable) {
+        messages <<- append(messages, msg)
+        if (level == "WARN") warnings <<- append(warnings, msg)
+      } else {
+        if (silentSuccess && !hasFailed) {
+          cat(paste(messages, collapse=""))
+          hasFailed <<- TRUE
+        }
+        cat(msg)
+      }
+
+      # Muffle any redundant output of the same message
+      optionalRestart = function(r) { res = findRestart(r); if (!is.null(res)) invokeRestart(res) }
+      optionalRestart("muffleMessage")
+      optionalRestart("muffleWarning")
+    }
+    vexpr = withCallingHandlers(withVisible(expr),
+                                debug=logger, message=logger, warning=logger, caughtError=logger, error=logger)
+    if (silentSuccess && !hasFailed) {
+      cat(paste(warnings, collapse=""))
+    }
+    if (vexpr$visible) vexpr$value else invisible(vexpr$value)
+  }
   #
   # define the environment for the cluster
   #
@@ -196,54 +245,6 @@ parallel.invoke.gpxp <- function (
       parallel::stopCluster(cl)
     })
   #
-  withJavaLogging <- function(expr, silentSuccess=FALSE, stopIsFatal=TRUE) {
-    hasFailed = FALSE
-    messages = list()
-    warnings = list()
-    logger = function(obj) {
-      # Change behaviour based on type of message
-      level = sapply(class(obj), switch, debug="DEBUG", message="INFO", warning="WARN", caughtError = "ERROR",
-                     error=if (stopIsFatal) "FATAL" else "ERROR", "")
-      level = c(level[level != ""], "ERROR")[1]
-      simpleMessage = switch(level, DEBUG=,INFO=TRUE, FALSE)
-      quashable = switch(level, DEBUG=,INFO=,WARN=TRUE, FALSE)
 
-      # Format message
-      time  = format(Sys.time(), "%Y-%m-%d %H:%M:%OS3")
-      txt   = conditionMessage(obj)
-      if (!simpleMessage) txt = paste(txt, "\n", sep="")
-      msg = paste(time, level, txt, sep=" ")
-      calls = sys.calls()
-      calls = calls[1:length(calls)-1]
-      trace = limitedLabels(c(calls, attr(obj, "calls")))
-      if (!simpleMessage && length(trace) > 0) {
-        trace = trace[length(trace):1]
-        msg = paste(msg, "  ", paste("at", trace, collapse="\n  "), "\n", sep="")
-      }
-
-      # Output message
-      if (silentSuccess && !hasFailed && quashable) {
-        messages <<- append(messages, msg)
-        if (level == "WARN") warnings <<- append(warnings, msg)
-      } else {
-        if (silentSuccess && !hasFailed) {
-          cat(paste(messages, collapse=""))
-          hasFailed <<- TRUE
-        }
-        cat(msg)
-      }
-
-      # Muffle any redundant output of the same message
-      optionalRestart = function(r) { res = findRestart(r); if (!is.null(res)) invokeRestart(res) }
-      optionalRestart("muffleMessage")
-      optionalRestart("muffleWarning")
-    }
-    vexpr = withCallingHandlers(withVisible(expr),
-                                debug=logger, message=logger, warning=logger, caughtError=logger, error=logger)
-    if (silentSuccess && !hasFailed) {
-      cat(paste(warnings, collapse=""))
-    }
-    if (vexpr$visible) vexpr$value else invisible(vexpr$value)
-  }
 }
 
