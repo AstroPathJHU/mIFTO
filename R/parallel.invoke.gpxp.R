7c493aa21fb00a4455980e404cc8f1395b694e1b
ssotodi1 <54324393+ssotodi1@users.noreply.github.com>
2023-01-10 11:22:08 -0500

    traceback

--- R/parallel.invoke.gpxp.R
+++ R/parallel.invoke.gpxp.R
@@ -108,12 +108,14 @@ parallel.invoke.gpxp <- function (
     small.tables.byimage<- tryCatch({
 
       tryCatch({
+        withJavaLogging({
         parallel::parLapply(
           cl,Image.IDs[[x]][[y]],function(z) mIFTO::generate.pxp.image.data(
             Concentration, x, y, z, Antibody_Opal,
             titration.type.name, Thresholds, paths,
             connected.pixels, flowout, Opal1,
             decile.logical, threshold.logical))
+        }, stopIsFatal=FALSE)
       }, warning = function(cond) {
         print("WARNING")
         print(cond)
@@ -124,12 +126,14 @@ parallel.invoke.gpxp <- function (
           cl <- parallel::makeCluster(
             getOption("cl.cores", numcores), useXDR = FALSE, methods = FALSE)
         }
+        withJavaLogging({
         parallel::parLapply(
           cl,Image.IDs[[x]][[y]],function(z) mIFTO::generate.pxp.image.data(
             Concentration, x, y, z, Antibody_Opal,
             titration.type.name, Thresholds, paths,
             connected.pixels, flowout, Opal1,
             decile.logical, threshold.logical))
+        }, stopIsFatal=FALSE)
       }, error = function(cond) {
         print("ERROR")
         print(cond)
@@ -140,12 +144,14 @@ parallel.invoke.gpxp <- function (
           cl <- parallel::makeCluster(
             getOption("cl.cores", numcores), useXDR = FALSE, methods = FALSE)
         }
+        withJavaLogging({
         parallel::parLapply(
           cl,Image.IDs[[x]][[y]],function(z) mIFTO::generate.pxp.image.data(
             Concentration, x, y, z, Antibody_Opal,
             titration.type.name, Thresholds, paths,
             connected.pixels, flowout, Opal1,
             decile.logical, threshold.logical))
+        }, stopIsFatal=FALSE)
       }, finally={
         print("FINALLY")
       }
@@ -153,6 +159,7 @@ parallel.invoke.gpxp <- function (
 
 
     }, warning = function(cond) {
+
       print(cond)
       modal_out <- shinyalert::shinyalert(
         title = paste0('Warning in parallel invoke Reading Component Images for ',
@@ -169,6 +176,8 @@ parallel.invoke.gpxp <- function (
       err.val <- 14
       return(err.val)
     }, error = function(cond) {
+      traceback(x = NULL, max.lines = getOption("traceback.max.lines",
+                                                getOption("deparse.max.lines", -1L)))
       print(cond)
       modal_out <- shinyalert::shinyalert(
         title = paste0('Error in parallel invoke Reading Component Images for ',
@@ -189,4 +198,53 @@ parallel.invoke.gpxp <- function (
       parallel::stopCluster(cl)
     })
   #
+}
+withJavaLogging = function(expr, silentSuccess=FALSE, stopIsFatal=TRUE) {
+  hasFailed = FALSE
+  messages = list()
+  warnings = list()
+  logger = function(obj) {
+    # Change behaviour based on type of message
+    level = sapply(class(obj), switch, debug="DEBUG", message="INFO", warning="WARN", caughtError = "ERROR",
+                   error=if (stopIsFatal) "FATAL" else "ERROR", "")
+    level = c(level[level != ""], "ERROR")[1]
+    simpleMessage = switch(level, DEBUG=,INFO=TRUE, FALSE)
+    quashable = switch(level, DEBUG=,INFO=,WARN=TRUE, FALSE)
+
+    # Format message
+    time  = format(Sys.time(), "%Y-%m-%d %H:%M:%OS3")
+    txt   = conditionMessage(obj)
+    if (!simpleMessage) txt = paste(txt, "\n", sep="")
+    msg = paste(time, level, txt, sep=" ")
+    calls = sys.calls()
+    calls = calls[1:length(calls)-1]
+    trace = limitedLabels(c(calls, attr(obj, "calls")))
+    if (!simpleMessage && length(trace) > 0) {
+      trace = trace[length(trace):1]
+      msg = paste(msg, "  ", paste("at", trace, collapse="\n  "), "\n", sep="")
+    }
+
+    # Output message
+    if (silentSuccess && !hasFailed && quashable) {
+      messages <<- append(messages, msg)
+      if (level == "WARN") warnings <<- append(warnings, msg)
+    } else {
+      if (silentSuccess && !hasFailed) {
+        cat(paste(messages, collapse=""))
+        hasFailed <<- TRUE
+      }
+      cat(msg)
+    }
+
+    # Muffle any redundant output of the same message
+    optionalRestart = function(r) { res = findRestart(r); if (!is.null(res)) invokeRestart(res) }
+    optionalRestart("muffleMessage")
+    optionalRestart("muffleWarning")
+  }
+  vexpr = withCallingHandlers(withVisible(expr),
+                              debug=logger, message=logger, warning=logger, caughtError=logger, error=logger)
+  if (silentSuccess && !hasFailed) {
+    cat(paste(warnings, collapse=""))
+  }
+  if (vexpr$visible) vexpr$value else invisible(vexpr$value)
 }
